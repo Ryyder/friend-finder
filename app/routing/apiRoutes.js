2051c37109adf0ee8f607beabb98d7b9a783f6db
Ryyder <brucecho82@hotmail.com>
2019-05-19 15:05:04 -0700

    updated survey and index.html also finished friend-finder algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,5 +9,31 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
     console.log(req.body.scores);
 
+    var newUser = req.body;
+
+    //convert our score strings to integers
+    for(var i = 0; i < newUser.scores.length; i++) {
+      newUser.scores[i] = parseInt(user.scores[i]);
+    }
+    
+  var bestMatch;
+  var currentBest = 100;
+  var maxDiff = [];
+  for (i = 0; i < friends.length; i++) {
+    var currentFriend = friends[i].scores;
+    console.log(currentFriend)
+    for (var j = 0; j < currentFriend.length; j++) {
+       maxDiff.push(Math.abs(currentFriend[j] - testArr[j]));
+       var total =  maxDiff.reduce((a, b) => a + b)
+    }
+    
+    maxDiff = [];
+    if(total < currentBest) {
+     currentBest = total;
+     bestMatch = friends[i];
+    }
+    
+  }
+
   });
 };
